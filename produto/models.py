321b3777a35a1f0766e7915c4df3c7e7d7e301ce
LucasCorpT5 <lucas.contatoestud@gmail.com>
2022-07-15 14:54:12 -0300

    models_db_upd1

--- produto/models.py
+++ produto/models.py
@@ -1,3 +1,4 @@
+from distutils.command.upload import upload
 from django.db import models
 
 # Create your models here.
@@ -10,3 +11,19 @@ class Categoria(models.Model):
 class Opcoes(models.Model):
     nome = models.CharField(max_length=200)
     acrecimo = models.FloatField(default=0)
+
+class Adicional(models.Model):
+    nome = models.CharField(max_lenght=100, unique=True)
+    maximo = models.IntegerField()
+    minimo = models.IntegerField()
+    opcoes = models.ManyToManyField(Opcoes)
+
+class Produto(models.Model):
+    nome_produto = models.CharField(max_length=200)
+    img = models.ImageField(upload_to='post_img')
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    preco = models.FloatField()
+    descricao = models.TextField() # sem limite de caracteres
+    ingredientes = models.CharField(max_length=2000)
+    adicionais = models.ManyToManyField(Adicional, blank=True)
+    ativo = models.BooleanField(default=True)
