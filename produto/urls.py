61dea18f437368de48d8122f63e755ac3db379c3
LucasCorpT5 <lucas.contatoestud@gmail.com>
2022-07-18 15:26:36 -0300

    add_carrinho_upd18

--- produto/urls.py
+++ produto/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('categoria/<int:id>', views.categorias, name="categoria"),
     path('produto/<int:id>', views.produto, name="produto"),
     path('add_carrinho/', views.add_carrinho, name="add_carrinho"),
-    path('ver_carrinho/', views.ver_carrinho, name="ver_carrinho")
+    path('ver_carrinho/', views.ver_carrinho, name="ver_carrinho"),
+    path('remover_carrinho/', views.remover_carrinho, name="remover_carrinho")
 ]
