d32a9642640793be9c24ecf34f844ab52569d144
LucasCorpT5 <lucas.contatoestud@gmail.com>
2022-07-18 18:37:53 -0300

    admin_pedido_upd2

--- pedido/admin.py
+++ pedido/admin.py
@@ -6,3 +6,14 @@ class itemPedidoInline(admin.TabularInline):
     readonly_fields = ('produto', 'quantidade', 'preco', 'descricao', 'adicionais',)
     model = ItemPedido
     extra = 1
+
+class PedidoAdmin(admin.ModelAdmin):
+    inlines = [
+        itemPedidoInline
+    ]
+    list_display = ('usuario', 'total', 'data', 'entregue')
+    search_fields = ('entregue',)
+    readonly_fields = ('usuario', 'total', 'troco', 'pagamento', 'ponto_referencia', 'data', 'cep', 'rua', 'numero', 'bairro', 'telefone')
+    list_filter = ('entregue',)
+
+admin.site.register(Pedido, PedidoAdmin)
