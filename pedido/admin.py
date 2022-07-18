f7b1148418ad742f183defa5dbbd7850168c35d3
LucasCorpT5 <lucas.contatoestud@gmail.com>
2022-07-18 17:40:46 -0300

    admin_pedido_upd1

--- pedido/admin.py
+++ pedido/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Pedido, ItemPedido
 
 # Register your models here.
+class itemPedidoInline(admin.TabularInline):
+    readonly_fields = ('produto', 'quantidade', 'preco', 'descricao', 'adicionais',)
+    model = ItemPedido
+    extra = 1
