fc662240c8411bb94654ae7dace1867b1194458d
LucasCorpT5 <lucas.contatoestud@gmail.com>
2022-07-18 14:59:46 -0300

    add_carrinho_upd17

--- phoenixdelivery/settings.py
+++ phoenixdelivery/settings.py
@@ -65,6 +65,10 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
+
+            'libraries': {
+                'filtros': 'produto.templatetags.filtros'
+            }
         },
     },
 ]
