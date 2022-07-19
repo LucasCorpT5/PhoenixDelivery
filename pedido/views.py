f86dd1cc0d7c1f19d2d1f5ef28f36db95e73fa0c
LucasCorpT5 <lucas.contatoestud@gmail.com>
2022-07-19 20:39:20 -0300

    cupom_descontos_upd8

--- pedido/views.py
+++ pedido/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
+from produto.models import Categoria
+
 # Create your views here.
+def finalizar_pedido(request):
+    if request.method == "GET":
+        categorias = Categoria.objects.all()
+
+def validaCupom(request):
+    pass
